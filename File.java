4310cc50f233fdf8d69608b3f6eb3a884e7a3b66
Avinash Baghel <68183276+avi-code03@users.noreply.github.com>
2020-08-11 16:50:24 +0530

    ReqNo:8:  Fixed String args passed

--- File.java
+++ File.java
@@ -1,13 +1,15 @@
-Public class{
+Public class File{
 	public static void main(){
 		System.out.println("Hello Java");
 	}
 	
-	public void test(){
-		System.out.println("Hello from test method");
+	public void test(String str){
+		System.out.println(str+" Hello from test method");
 	}
 
 	public void newTest(){
+		File f = new File();
+		f.test("Hello Git");
 		System.out.println("Hello from newTest method");
 	}
 }
